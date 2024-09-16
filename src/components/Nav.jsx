bc4d70b281b22b88e3cf0719ea4c5385d72dcdfe
JHS <118969653+ARISTAHS@users.noreply.github.com>
2024-09-17 01:12:19 +0900

    기본퍼블리싱

---
+++ src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import '../style/Nav.css'
+
+function Nav(){
+  return(
+    <>
+      <ul className='nav'>
+        <li><a href="/">Nav1</a></li>
+        <li><a href="/">Nav2</a></li>
+        <li><a href="/">Nav3</a></li>
+      </ul>
+    </>
+  );
+}
+
+export default Nav;
