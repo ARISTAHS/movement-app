58b58ed69bd3b567f8726c49ddc44c5452b7c9f2
ARISTAHS <paran0907@naver.com>
2024-09-18 20:50:13 +0900

    브라우저 라우터 및 라우터 링크 확인

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,20 @@
+import { Link, Route,Switch } from 'react-router-dom';
 import '../style/Nav.css'
 
 function Nav(){
   return(
     <>
       <ul className='nav'>
-        <li><a href="/">Nav1</a></li>
-        <li><a href="/">Nav2</a></li>
-        <li><a href="/">Nav3</a></li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/Nav2">Nav2</Link></li>
+        <li><Link to="/Nav3">Nav3</Link></li>
       </ul>
+
+      <Switch>
+        <Route exact path='/'>Home</Route>
+        <Route path='/Nav2'>네비2 관련 content 내용</Route>
+        <Route path='/Nav3'>네비3 관련 content 내용</Route>
+      </Switch>
     </>
   );
 }
