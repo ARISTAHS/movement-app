a028c5080ba8cb06432158cf8e0b1c6f6826a3da
JHS <118969653+ARISTAHS@users.noreply.github.com>
2024-10-05 23:11:14 +0900

    에러페이지 링크

--- src/components/Error/ErrorPage.jsx
+++ src/components/Error/ErrorPage.jsx
@@ -5,7 +5,7 @@ function ErrorPage(){
   return(
     <>
       <h1 className={styles.title}>에러페이지입니다.</h1>
-      <Link to='/Home'></Link>
+      <Link to='/Home'>여기를 눌러주세요!</Link>
     </>
   );
 }
