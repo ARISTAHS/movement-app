d1fbfc28379a47d7bfad80b244efae0ab97451e2
ARISTAHS <paran0907@naver.com>
2024-10-10 20:54:50 +0900

    home 화면 시작

--- src/components/Content/Home/Home.jsx
+++ src/components/Content/Home/Home.jsx
@@ -3,6 +3,9 @@ import styles from './Home.module.css';
 function Home(){
   return(
     <>
+      <div className="visualImg">
+        
+      </div>
       <h2 className={styles.title}>Home 입니다. 화면 표시용 테스트</h2>
     </>
   );
